4c92a5b06a5d8dd1eade841bfd15f886cb4ac841
manu <manuelmonjes@gmail.com>
2024-04-04 17:23:19 -0300

    add view login and route api/login get and post

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import path from 'path'
 const app = express();
 const PORT = 3000;
 
@@ -10,6 +10,18 @@ app.get('/api', (req, res) => {
 	//se puede enviar el por separado los res.status/ header y send siempre y cuando el ultimo sea el metodo send
 })
 
+const __dirname = path.resolve()
+app.use(express.static(__dirname))
+//primero definimos la ruta absouta del directorio para no poner el tremendo chorizo en sendFile y como con modulejs no esta definido __dirname usarmos path con el metodo de resolve si no se le pasa un parametro devuelve la ruta absoluta de donde se ejecuta me sirve ya que mi index.html esta en la misma carpeta.
+
+app.get('/api/login', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
+})
+
+app.use(express.json())
+app.post('/api/login', (req, res) => {
+	console.log(req.body)
+})
 
 
 app.listen(PORT, () => {
