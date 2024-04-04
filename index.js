acb90ef07438ef5d566bca9f619a56adaf24c92b
manu <manuelmonjes@gmail.com>
2024-04-04 16:09:34 -0300

    path /api ok with requirements

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ import express from 'express'
 
 const app = express();
 const PORT = 3000;
-app.get('/', (req, res) => {
-	res.send('Hello World!!!')
+
+//ok
+app.get('/api', (req, res) => {
+	res.status(200).setHeader('Content-Type', 'text/plain').send('Hello World!')
+	//si aparece un 304 es porque los archivos los esta cargando desde la cache no es un error indica que los datos almacenados en memoria del navegador estan ok y puede seguir utilizandolos
+	//se puede enviar el por separado los res.status/ header y send siempre y cuando el ultimo sea el metodo send
 })
+
+
 
 app.listen(PORT, () => {
 	console.log(`Escuchando en el puerto ${PORT}`)
