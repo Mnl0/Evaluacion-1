637d3bf580820c850bddeb56f318f8ad529f2a30
manu <manuelmonjes@gmail.com>
2024-04-05 10:00:54 -0300

    initial commit with proyect base

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { scrypt, randomBytes, randomUUID } from 'node:crypto'
+
+const app = express()
+
+const users = [{
+	username: 'admin',
+	name: 'Gustavo Alfredo Marín Sáez',
+	password: '1b6ce880ac388eb7fcb6bcaf95e20083:341dfbbe86013c940c8e898b437aa82fe575876f2946a2ad744a0c51501c7dfe6d7e5a31c58d2adc7a7dc4b87927594275ca235276accc9f628697a4c00b4e01' // certamen123
+}]
+const todos = []
+
+app.use(express.static('public'))
+
+// Su código debe ir aquí...
+
+
+
+// ... hasta aquí
+
+export default app
