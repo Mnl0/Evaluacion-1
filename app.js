fdb09533dbdf27f87ac4865d66c9a0d203c28c1b
manu <manuelmonjes@gmail.com>
2024-04-30 14:35:10 -0400

    add ejs

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.use(express.static('public'))
 // Su código debe ir aquí...
 app.use(express.json())
 app.use('/', routerTodo)
+app.set('view engine', 'ejs')
+app.get('/test', (req, res) => {
+	res.render('pages/index', { nombre: 'manuel' })
+})
 
 // ... hasta aquí
 
